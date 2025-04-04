b3af3f6aef1a029963824cd9202c43f68c170b49
Frinze <arellanofrinze@gmail.com>
2025-04-04 12:48:21 +0800

    sun-city

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('see-more-btn').addEventListener('click', function() {
+    var departments = document.querySelectorAll('.more-departments');
+    departments.forEach(function(department) {
+        department.style.display = 'block';
+    });
+    document.getElementById('see-more-btn').style.display = 'none'; 
+    document.getElementById('hide-btn').style.display = 'inline-block'; 
+});
+
+document.getElementById('hide-btn').addEventListener('click', function() {
+    var departments = document.querySelectorAll('.more-departments');
+    departments.forEach(function(department) {
+        department.style.display = 'none';
+    });
+    document.getElementById('see-more-btn').style.display = 'inline-block'; 
+    document.getElementById('hide-btn').style.display = 'none'; 
+});
